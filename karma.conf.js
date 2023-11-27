c812d03b928f8025aea701f5f7ecfef2958a72d9
wilson marutti <wilson.marutti@gmail.com>
2023-11-27 20:53:35 -0300

    ajust bug sonar

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    browsers: ['ChromeHeadless'],
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
